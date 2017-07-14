2a60664ae6720810fbd484586d7d6b435344f637
Liu Zhihao <haohaohihi@gmail.com>
2017-07-14 19:18:26 +0800

    Reformat Code

--- src/RemoveElement.java
+++ src/RemoveElement.java
@@ -6,8 +6,8 @@ import java.util.Arrays;
 public class RemoveElement {
     public static int removeElementWithOrder(int[] nums, int val) {
         int i = 0;
-        for(int j=0; j<nums.length; j++){
-            if(nums[j] != val){
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
                 nums[i++] = nums[j];
             }
         }
@@ -17,20 +17,19 @@ public class RemoveElement {
     public static int removeElementWithoutOrder(int[] nums, int val) {
         int i = 0;
         int n = nums.length;
-        while(i < n){
-            if(nums[i] == val){
+        while (i < n) {
+            if (nums[i] == val) {
                 //n表示有效长度，每找到一个val，有效长度减一
-                nums[i] = nums[n-1];
+                nums[i] = nums[n - 1];
                 n--;
-            }
-            else
+            } else
                 i++;
         }
         return n;
     }
 
-    public static void main(String[] args){
-        int[] nums = {3,4,5,2,1,4};
+    public static void main(String[] args) {
+        int[] nums = {3, 4, 5, 2, 1, 4};
         int s = RemoveElement.removeElementWithOrder(nums, 5);
         System.out.println(s);
         System.out.println(Arrays.toString(nums));
--- src/TwoSum.java
+++ src/TwoSum.java
@@ -6,8 +6,8 @@ import java.util.HashMap;
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            if(map.containsKey(target - nums[i]))
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]))
                 return new int[]{map.get(target - nums[i]), i};
             else
                 map.put(nums[i], i);
